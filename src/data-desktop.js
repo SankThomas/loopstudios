a97c361af8f9fdc1c093d2dc61cbb3ce2d44caeb
Thomas Sankara <thomassankara74@gmail.com>
2021-01-09 10:56:31 +0300

    data arrays for Creations component

---
+++ src/data-desktop.js
@@ -0,0 +1,44 @@
+const data = [
+  {
+    id: 1,
+    title: 'Deep earth',
+    image: './images/desktop/image-deep-earth.jpg',
+  },
+  {
+    id: 2,
+    title: 'Night arcade',
+    image: './images/desktop/image-night-arcade.jpg',
+  },
+  {
+    id: 3,
+    title: 'Soccer team vr',
+    image: './images/desktop/image-soccer-team.jpg',
+  },
+  {
+    id: 4,
+    title: 'The grid',
+    image: './images/desktop/image-grid.jpg',
+  },
+  {
+    id: 5,
+    title: 'From up above vr',
+    image: './images/desktop/image-from-above.jpg',
+  },
+  {
+    id: 6,
+    title: 'Pocket borealis',
+    image: './images/desktop/image-pocket-borealis.jpg',
+  },
+  {
+    id: 7,
+    title: 'The curiosity',
+    image: './images/desktop/image-curiosity.jpg',
+  },
+  {
+    id: 8,
+    title: 'Make it fisheye',
+    image: './images/desktop/image-fisheye.jpg',
+  },
+]
+
+export default data
---
+++ src/data-mobile.js
@@ -0,0 +1,44 @@
+const data = [
+  {
+    id: 1,
+    title: 'Deep earth',
+    image: './images/mobile/image-deep-earth.jpg',
+  },
+  {
+    id: 2,
+    title: 'Night arcade',
+    image: './images/mobile/image-night-arcade.jpg',
+  },
+  {
+    id: 3,
+    title: 'Soccer team vr',
+    image: './images/mobile/image-soccer-team.jpg',
+  },
+  {
+    id: 4,
+    title: 'The grid',
+    image: './images/mobile/image-grid.jpg',
+  },
+  {
+    id: 5,
+    title: 'From up above vr',
+    image: './images/mobile/image-from-above.jpg',
+  },
+  {
+    id: 6,
+    title: 'Pocket borealis',
+    image: './images/mobile/image-pocket-borealis.jpg',
+  },
+  {
+    id: 7,
+    title: 'The curiosity',
+    image: './images/mobile/image-curiosity.jpg',
+  },
+  {
+    id: 8,
+    title: 'Make it fisheye',
+    image: './images/mobile/image-fisheye.jpg',
+  },
+]
+
+export default data
